1c23e896de0159f7c5c881a8245161ba8c62f2fc
iramos5 <ismael.ramos@iesnervion.es>
2022-10-06 23:12:55 +0200

    prueba2

--- EjerciciosTema2/src/ejerciciostema2/Ejercicio1.java
+++ EjerciciosTema2/src/ejerciciostema2/Ejercicio1.java
@@ -5,6 +5,7 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("prueba");
+		System.out.println("prueba 2");
 	}
 
 }
