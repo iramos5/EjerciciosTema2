f5b6aaff9816eb2c32f40f9c5b167d529ecc3546
iramos5 <ismael.ramos@iesnervion.es>
2022-10-06 23:22:29 +0200

    ejercicio 1

--- EjerciciosTema2/src/ejerciciostema2/Ejercicio1.java
+++ EjerciciosTema2/src/ejerciciostema2/Ejercicio1.java
@@ -1,11 +1,30 @@
 package ejerciciostema2;
+import java.util.Scanner;
 
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("prueba");
-		System.out.println("prueba 2");
+		//Diseñar una aplicación que solicite al usuario un número e indique si es par o impar.
+		
+		//variables
+		Scanner scan = new Scanner (System.in); //iniciar escáner
+		int num; //número a introducir
+		
+		//pedir datos
+		System.out.println("Introduce un número: "); //pedir el número al usuario
+		num = scan.nextInt(); // guardar el número introducido en la variable
+		
+		//comprobar si es par y mostrar resultado
+		if (num%2==0) { //Si el resto del número entre dos da cero
+			System.out.println("El número " + num + " es par"); // es par y se muestra por pantalla
+		}
+		else { //sino
+			System.out.println("El número " + num + " es impar"); //es impar y se muestra por pantalla
+		}
+		
+		// cierre escáner
+		scan.close();
+	
 	}
 
 }
