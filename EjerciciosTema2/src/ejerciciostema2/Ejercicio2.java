6eca3848fc9786d6f7c5e299618b618b06fb5c47
iramos5 <ismael.ramos@iesnervion.es>
2022-10-06 23:24:19 +0200

    ejercicio 2

---
+++ EjerciciosTema2/src/ejerciciostema2/Ejercicio2.java
@@ -0,0 +1,11 @@
+package ejerciciostema2;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// Pedir dos números enteros y decir si son iguales o no.
+
+	}
+
+}
