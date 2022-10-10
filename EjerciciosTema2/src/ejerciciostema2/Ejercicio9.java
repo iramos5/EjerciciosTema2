39b040ed48c71d0b71b3f22d4beffed05a63e19d
iramos <iramos@106-24.info2>
2022-10-10 08:42:27 +0200

    prueba

--- EjerciciosTema2/src/ejerciciostema2/Ejercicio9.java
+++ EjerciciosTema2/src/ejerciciostema2/Ejercicio9.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 public class Ejercicio9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* Escribir un programa que simule el juego de PIEDRA, PAPEL, TIJERA, 
+		 * pidiendo a cada jugador que escriba PIEDRA, PAPEL o TIJERA. 
+		 * El juego debe mostrar por pantalla quién ha ganado el juego tras jugar una partida. 
+		 * Hay que contemplar el caso de que empaten.
+		 */
+		
+		// variables
+		Scanner scan = new Scanner (System.in); //inciar escáner
+		
 
 	}
 
