268b36bf0e1d1ff5bc29a0c83b4a20e0db0d3095
iramos <iramos@106-24.info2>
2022-10-10 09:06:31 +0200

    ejercicio 9 acabado

--- EjerciciosTema2/src/ejerciciostema2/Ejercicio9.java
+++ EjerciciosTema2/src/ejerciciostema2/Ejercicio9.java
@@ -12,16 +12,31 @@ public class Ejercicio9 {
 		
 		// variables
 		Scanner scan = new Scanner (System.in); //inciar escáner
-		String jugador1, jugador2; // jugadores
-		String PIEDRA = "PIEDRA", PAPEL = "PAPEL", TIJERAS = "TIJERAS"; // jugadas
+		int jugador1, jugador2; // jugadores
+		final int PIEDRA = 1, PAPEL = 2, TIJERAS = 3; // jugadas
 		
 		// introducir datos
-		System.out.println("Jugador 1: Escribe PIEDRA, PAPEL o TIJERAS: ");
-		jugador1 = scan.next();
-		System.out.println("Jugador 2: Escribe PIEDRA, PAPEL o TIJERAS: ");
-		jugador2 = scan.next();
+		System.out.println("Jugador 1: Escribe PIEDRA (1), PAPEL (2) o TIJERAS (3): "); // meter jugador 1
+		jugador1 = scan.nextInt(); // guardar jugador 1
+		System.out.println("Jugador 2: Escribe PIEDRA (1), PAPEL (2) o TIJERAS (3): "); // meter jugador 2
+		jugador2 = scan.nextInt(); // guardar jugador 2
 		
 		// juego
+		if (jugador1 == jugador2) { // si el jugador 1 es igual al jugador 2, hay un empate
+			System.out.println("Empate");
+		}
+		else if (jugador1 == 1 && jugador2 ==3 || jugador1 == 2 && jugador2 == 1 || jugador1 == 3 && jugador2 ==2) { 
+			System.out.println("Gana el jugador 1"); // opciones en las que ganaría el jugador 1
+		}
+		else if (jugador1 < 1 || jugador1 > 3 || jugador2 <1 || jugador2 >3) { // datos introducidos que no serían válidos
+			System.out.println("Los datos introducidos no son válidos");
+		}
+		else {
+			System.out.println("Gana jugador 2"); // todo lo demás ganaría el jugador 2
+		}
+		
+		// cierre escáner
+		scan.close();
 		
 		
 		
